0dcd64b9731f383294dbf32f77cb91b9ee272dd7
dinopatates <150128493+dinopatates@users.noreply.github.com>
2024-02-13 16:58:05 +0100

    Palette de couleurs (pas encore fonctionnelle)
    La palette de couleurs ne marche pas, il faudrait sûrement utiliser un if, else pour que cela marche. Pour l'instant, il n'y a que le dernier attribut ( la dernière couleur) qui est pris en compte.

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
+const blackColor = document.getElementById("blackColorPick")
+const pinkColor = document.getElementById("pinkColorPick")
+const black = "#000000";
+const pink = "#FFC0CB";
+
 
 
 let coord = { x: 0, y: 0 };
@@ -14,6 +19,7 @@ function reposition(event) {
 
 function start(event) {
   document.addEventListener("mousemove", draw);
+  console.log("The pen's working")
   reposition(event);
 }
 
@@ -26,7 +32,19 @@ function draw(event) {
 
   ctx.lineWidth = 5;
   ctx.lineCap = "round";
-  ctx.strokeStyle = "#ACD3ED";
+
+  pinkColor.addEventListener("click", function click() {
+
+    console.log("The pen is now pink")
+    ctx.stroke.Style = pink;
+})
+ctx.stroke.Style = pink;
+
+blackColor.addEventListener("click", function click() {
+  console.log("The pen is now black")
+  ctx.strokeStyle = pink;
+})
+ctx.strokeStyle = pink;  // peut être utiliser un if else pour que cela marche étant donné qu'il ne prend en considération que la dernière attribution.
 
   ctx.moveTo(coord.x, coord.y);
   reposition(event);
@@ -34,6 +52,7 @@ function draw(event) {
   ctx.lineTo(coord.x, coord.y);
   ctx.stroke();
 }
+
 
 
 document.addEventListener("mousedown", start);
